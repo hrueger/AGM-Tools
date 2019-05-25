36e93ca0296a8861a83d782f377b6d1cf94530f3
Hannes Rüger <hannesrueger@gmx.de>
2019-05-25 18:33:47 +0200

    added templates show

--- AGM-Tools/src/app/_components/templates/templates.component.ts
+++ AGM-Tools/src/app/_components/templates/templates.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RemoteService } from "../../_services/remote.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import config from "../../_config/config";
+import { AuthenticationService } from "../../_services/authentication.service";
 
 @Component({
-  selector: 'app-templates',
-  templateUrl: './templates.component.html',
-  styleUrls: ['./templates.component.scss']
+    selector: "app-templates",
+    templateUrl: "./templates.component.html",
+    styleUrls: ["./templates.component.scss"]
 })
 export class TemplatesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(
+        private remoteService: RemoteService,
+        private modalService: NgbModal,
+        private authenticationService: AuthenticationService
+    ) {}
+    imgUrl: string;
+    templates: any;
+    ngOnInit() {
+        this.remoteService.get("templatesGetTemplates").subscribe(data => {
+            this.templates = data;
+        });
+    }
+    show(template, content) {
+        this.imgUrl =
+            config.apiUrl +
+            "getTemplate.php?tid=" +
+            template.id +
+            "&token=" +
+            this.authenticationService.currentUserValue.token;
+        this.modalService
+            .open(content, {})
+            .result.then(result => {}, reason => {});
+    }
 }
