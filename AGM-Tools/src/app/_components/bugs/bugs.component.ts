0277496a855f18f354166f2869221fca679f3a3d
Hannes Rüger <hannesrueger@gmx.de>
2019-05-25 18:33:13 +0200

    added display bugs

--- AGM-Tools/src/app/_components/bugs/bugs.component.ts
+++ AGM-Tools/src/app/_components/bugs/bugs.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RemoteService } from "../../_services/remote.service";
 
 @Component({
-  selector: 'app-bugs',
-  templateUrl: './bugs.component.html',
-  styleUrls: ['./bugs.component.scss']
+    selector: "app-bugs",
+    templateUrl: "./bugs.component.html",
+    styleUrls: ["./bugs.component.scss"]
 })
 export class BugsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(private remoteService: RemoteService) {}
+    bugs: any;
+    ngOnInit() {
+        this.remoteService.get("bugsGetBugs").subscribe(data => {
+            this.bugs = data;
+        });
+    }
 }
