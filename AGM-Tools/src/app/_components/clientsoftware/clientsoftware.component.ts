7e5f0fd9eb118dae0c61b895e1581adb0da0bb56
Hannes Rüger <hannesrueger@gmx.de>
2019-05-25 18:33:26 +0200

    added display clientsoftware

--- AGM-Tools/src/app/_components/clientsoftware/clientsoftware.component.ts
+++ AGM-Tools/src/app/_components/clientsoftware/clientsoftware.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RemoteService } from "../../_services/remote.service";
 
 @Component({
-  selector: 'app-clientsoftware',
-  templateUrl: './clientsoftware.component.html',
-  styleUrls: ['./clientsoftware.component.scss']
+    selector: "app-clientsoftware",
+    templateUrl: "./clientsoftware.component.html",
+    styleUrls: ["./clientsoftware.component.scss"]
 })
 export class ClientsoftwareComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(private remoteService: RemoteService) {}
+    apps: any;
+    desktopapps: any;
+    ngOnInit() {
+        this.remoteService.get("clientsoftwareGetMobile").subscribe(data => {
+            this.apps = data;
+        });
+    }
 }
