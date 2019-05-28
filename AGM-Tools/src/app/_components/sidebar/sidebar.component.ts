ae06433cb6a2e553f47f4b9c613cb17e5f62b3c1
Hannes Rüger <hannesrueger@gmx.de>
2019-05-28 18:20:45 +0200

    fixed icons

--- AGM-Tools/src/app/_components/sidebar/sidebar.component.ts
+++ AGM-Tools/src/app/_components/sidebar/sidebar.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-sidebar",
-  templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"]
+    selector: "app-sidebar",
+    templateUrl: "./sidebar.component.html",
+    styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
-  navLinks = [
-    { name: "Dashboard", icon: "tachometer", target: "dashboard" },
-    { name: "Benutzer", icon: "user", target: "users" },
-    { name: "Chat", icon: "comments", target: "chat" },
-    { name: "Benachrichtigungen", icon: "envelope", target: "notifications" },
-    { name: "Kalender", icon: "calendar", target: "calendar" },
-    { name: "Projekte", icon: "folder", target: "projects" },
-    { name: "Dateien", icon: "image", target: "files" },
-    { name: "Vorlagen", icon: "picture-o", target: "templates" },
-    {
-      name: "Fehler / Verbesserungen",
-      icon: "bug",
-      target: "bugs"
-    },
-    { name: "Fertig", icon: "check", target: "done" },
-    { name: "Client-Software", icon: "mobile", target: "clientsoftware" }
-  ];
-  constructor() {}
+    navLinks = [
+        { name: "Dashboard", icon: "tachometer-alt", target: "dashboard" },
+        { name: "Benutzer", icon: "user", target: "users" },
+        { name: "Chat", icon: "comments", target: "chat" },
+        {
+            name: "Benachrichtigungen",
+            icon: "envelope",
+            target: "notifications"
+        },
+        { name: "Kalender", icon: "calendar", target: "calendar" },
+        { name: "Projekte", icon: "folder", target: "projects" },
+        { name: "Dateien", icon: "image", target: "files" },
+        { name: "Vorlagen", icon: "portrait", target: "templates" },
+        {
+            name: "Fehler / Verbesserungen",
+            icon: "bug",
+            target: "bugs"
+        },
+        { name: "Fertig", icon: "check", target: "done" },
+        { name: "Client-Software", icon: "mobile", target: "clientsoftware" }
+    ];
+    constructor() {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 }
