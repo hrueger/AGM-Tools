eb68c83abd48bff272d28dbf913306378d07c1bf
Hannes Rüger <hannesrueger@gmx.de>
2019-09-01 21:37:45 +0200

    added docker image

---
+++ docker/README.md
@@ -0,0 +1,7 @@
+##ToDo
+
+Build:
+`docker build -t agmtools:v1 .`
+
+## Credits:
+https://github.com/ulsmith/alpine-apache-php7/
---
+++ docker/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$CONTAINER_TIMEZONE" ]; then
+    cp /usr/share/zoneinfo/${CONTAINER_TIMEZONE} /etc/localtime && \
+	echo "${CONTAINER_TIMEZONE}" >  /etc/timezone && \
+	echo "Container timezone set to: $CONTAINER_TIMEZONE"
+fi
+ntpd -s
+
+# Apache server name change
+if [ ! -z "$APACHE_SERVER_NAME" ]
+	then
+		sed -i "s/#ServerName www.example.com:80/ServerName $APACHE_SERVER_NAME/" /etc/apache2/httpd.conf
+		echo "Changed server name to '$APACHE_SERVER_NAME'..."
+	else
+		echo "NOTICE: Change 'ServerName' globally and hide server message by setting environment variable >> 'APACHE_SERVER_NAME=your.server.name' in docker command or docker-compose file"
+fi
+
+echo "Clearing any old processes..."
+rm -f /run/apache2/apache2.pid
+rm -f /run/apache2/httpd.pid
+
+echo "Starting apache..."
+httpd -D FOREGROUND
