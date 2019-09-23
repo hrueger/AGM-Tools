d47e1a3c3a121d9988eddcbd637855404d75dd50
Hannes Rüger <hannesrueger@gmx.de>
2019-09-23 20:22:38 +0200

    fixed error

--- AGM-Tools/src/polyfills.ts
+++ AGM-Tools/src/polyfills.ts
@@ -74,4 +74,4 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import "core-js/es7/reflect";
+//import "core-js/es7/reflect";
