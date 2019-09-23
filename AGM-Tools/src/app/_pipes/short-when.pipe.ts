4947d86ff4c2d1493ad7dd9fb69c8e0e4fc85801
Hannes Rüger <hannesrueger@gmx.de>
2019-09-23 20:23:00 +0200

    fixed date-fns breaking change error

--- AGM-Tools/src/app/_pipes/short-when.pipe.ts
+++ AGM-Tools/src/app/_pipes/short-when.pipe.ts
@@ -1,7 +1,7 @@
 import { DatePipe } from "@angular/common";
-import { Inject, LOCALE_ID, Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from "@angular/core";
 
-import { isToday, isYesterday, parse } from "date-fns";
+import { isToday, isYesterday, toDate } from "date-fns";
 
 @Pipe({
     name: "shortWhen",
@@ -10,18 +10,18 @@ import { isToday, isYesterday, parse } from "date-fns";
 export class ShortWhenPipe implements PipeTransform {
 
     public transform(value: string): string {
-        const datePipe = new DatePipe("de-DE");
-        // @ts-ignore
-        const parsedDate = parse(value);
-
-        if (isToday(parsedDate)) {
-            return datePipe.transform(parsedDate, "HH:mm");
+        try {
+            const datePipe = new DatePipe("de-DE");
+            const parsedDate = toDate(Date.parse(value));
+            if (isToday(parsedDate)) {
+                return datePipe.transform(parsedDate, "HH:mm");
+            }
+            if (isYesterday(parsedDate)) {
+                return "Gestern";
+            }
+            return datePipe.transform(parsedDate, "dd.MM.yyyy");
+        } catch {
+            return "";
         }
-
-        if (isYesterday(parsedDate)) {
-            return "Gestern";
-        }
-
-        return datePipe.transform(parsedDate, "dd.MM.yyyy");
     }
 }
