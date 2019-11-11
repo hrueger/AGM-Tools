6c719ba45bb4e33c20d61b04abc993704ef7c796
Hannes Rüger <hannesrueger@gmx.de>
2019-11-11 13:51:20 -1000

    added script for creating secret placeholder files

---
+++ AGM-Tools/createFilesWithApiKeys.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const files = {
+    "src/environments/environment.ts": 'export const environment = {firebase: {apiKey: "",appId: "",authDomain: "",databaseURL: "",messagingSenderId: "",projectId: "",storageBucket: ""},production: false};',
+    "src/environments/environment.prod.ts": 'export const environment = {firebase: {apiKey: "",appId: "",authDomain: "",databaseURL: "",messagingSenderId: "",projectId: "",storageBucket: ""},production: true};',
+    "src/firebase-messaging-sw.js": 'importScripts("https://www.gstatic.com/firebasejs/6.5.0/firebase-app.js");importScripts("https://www.gstatic.com/firebasejs/6.5.0/firebase-messaging.js");firebase.initializeApp({"messagingSenderId": ""});const messaging = firebase.messaging();',
+    "App_Resources/Android/google-services.json": '{"project_info":{"project_number":"","firebase_url":"","project_id":"","storage_bucket":""},"client":[{"client_info":{"mobilesdk_app_id":"","android_client_info":{"package_name":""}},"oauth_client":[{"client_id":"","client_type":3}],"api_key":[{"current_key":""}],"services":{"appinvite_service":{"other_platform_oauth_client":[{"client_id":"","client_type":3}]}}}],"configuration_version":"1"}'
+};
+for (key in files) {
+    if (!fs.existsSync(key)) {
+        fs.writeFileSync(key, files[key]);
+        console.info("File", key, "created, please insert your credentials there!");
+    } else {
+        console.info("File", key, "skipped, as it already exists!");
+    }
+}
