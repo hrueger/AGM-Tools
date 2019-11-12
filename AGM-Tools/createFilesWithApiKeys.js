49b332111b6331c35079125b590d4f557c280866
Hannes Rüger <hannesrueger@gmx.de>
2019-11-11 20:42:08 -1000

    added support for overwriting file contents

--- AGM-Tools/createFilesWithApiKeys.js
+++ AGM-Tools/createFilesWithApiKeys.js
@@ -6,11 +6,17 @@ const files = {
     "src/firebase-messaging-sw.js": 'importScripts("https://www.gstatic.com/firebasejs/6.5.0/firebase-app.js");importScripts("https://www.gstatic.com/firebasejs/6.5.0/firebase-messaging.js");firebase.initializeApp({"messagingSenderId": ""});const messaging = firebase.messaging();',
     "App_Resources/Android/google-services.json": '{"project_info":{"project_number":"","firebase_url":"","project_id":"","storage_bucket":""},"client":[{"client_info":{"mobilesdk_app_id":"","android_client_info":{"package_name":"de.multimediaag.agmtools"}},"oauth_client":[{"client_id":"","client_type":3}],"api_key":[{"current_key":""}],"services":{"appinvite_service":{"other_platform_oauth_client":[{"client_id":"","client_type":3}]}}}],"configuration_version":"1"}'
 };
+var args = process.argv.slice(2);
+counter = 0;
 for (key in files) {
     if (!fs.existsSync(key)) {
+        if (args[counter]) {
+            files[key] = args[counter];
+        }
         fs.writeFileSync(key, files[key]);
         console.info("File", key, "created, please insert your credentials there!");
     } else {
         console.info("File", key, "skipped, as it already exists!");
     }
+    counter = counter++;
 }
