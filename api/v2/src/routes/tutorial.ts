c5bf40bc54463372df70b7db9e990073f54614d1
Hannes Rüger <hannesrueger@gmx.de>
2019-11-18 20:15:37 -1000

    added missing jwt token middleware

--- api/v2/src/routes/tutorial.ts
+++ api/v2/src/routes/tutorial.ts
@@ -7,13 +7,13 @@ const router = Router();
 router.get("/", [checkJwt], TutorialController.listAll);
 router.post("/", [checkJwt], TutorialController.newTutorial);
 
-router.get("/:id([0-9]+)", TutorialController.getTutorial);
-router.post("/:id([0-9]+)", TutorialController.updateTutorial);
+router.get("/:id([0-9]+)", [checkJwt], TutorialController.getTutorial);
+router.post("/:id([0-9]+)", [checkJwt], TutorialController.updateTutorial);
 
-router.post("/:tutorialId([0-9]+)/step", TutorialController.createStep);
-router.post("/:tutorialId([0-9]+)/step/:id([0-9]+)", TutorialController.updateStep);
-router.delete("/:tutorialId([0-9]+)/step/:id([0-9]+)", TutorialController.deleteStep);
-router.post("/:tutorialId([0-9]+)/step/:id([0-9]+)/image", TutorialController.uploadImage);
-router.get("/image/:filename", TutorialController.viewImage);
+router.post("/:tutorialId([0-9]+)/step", [checkJwt], TutorialController.createStep);
+router.post("/:tutorialId([0-9]+)/step/:id([0-9]+)", [checkJwt], TutorialController.updateStep);
+router.delete("/:tutorialId([0-9]+)/step/:id([0-9]+)", [checkJwt], TutorialController.deleteStep);
+router.post("/:tutorialId([0-9]+)/step/:id([0-9]+)/image", [checkJwt], TutorialController.uploadImage);
+router.get("/image/:filename", [checkJwt], TutorialController.viewImage);
 
 export default router;
