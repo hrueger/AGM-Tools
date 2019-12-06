8f4b8279281142171d5e787608e5e09c83a59a78
Hannes Rüger <hannesrueger@gmx.de>
2019-12-05 17:30:23 -1000

    fixed linting error

--- api/v2/src/entity/User.ts
+++ api/v2/src/entity/User.ts
@@ -1,92 +1,92 @@
-  import * as bcrypt from "bcryptjs";
-  import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    ManyToMany,
-    ManyToOne,
-    OneToMany,
-    PrimaryGeneratedColumn,
-    Unique,
-    UpdateDateColumn,
-  } from "typeorm";
-  import { Event } from "./Event";
-  import { File } from "./File";
-  import { Notification } from "./Notification";
-  import { Project } from "./Project";
-  import { Template } from "./Template";
-  import { Tutorial } from "./Tutorial";
-  import { Usergroup } from "./Usergroup";
+import * as bcrypt from "bcryptjs";
+import {
+Column,
+CreateDateColumn,
+Entity,
+ManyToMany,
+ManyToOne,
+OneToMany,
+PrimaryGeneratedColumn,
+Unique,
+UpdateDateColumn,
+} from "typeorm";
+import { Event } from "./Event";
+import { File } from "./File";
 import { Message } from "./Message";
+import { Notification } from "./Notification";
+import { Project } from "./Project";
+import { Template } from "./Template";
+import { Tutorial } from "./Tutorial";
+import { Usergroup } from "./Usergroup";
 
-  @Entity()
-  @Unique(["username"])
-  export class User {
-    @PrimaryGeneratedColumn()
-    public id: number;
+@Entity()
+@Unique(["username"])
+export class User {
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @Column()
-    public username: string;
+  @Column()
+  public username: string;
 
-    @Column()
-    public email: string;
+  @Column()
+  public email: string;
 
-    @Column()
-    public password: string;
+  @Column()
+  public password: string;
 
-    @Column()
-    public passwordResetToken: string;
+  @Column()
+  public passwordResetToken: string;
 
-    @ManyToOne((type) => Usergroup, (usergroup) => usergroup.users)
-    public usergroup: Usergroup;
+  @ManyToOne((type) => Usergroup, (usergroup) => usergroup.users)
+  public usergroup: Usergroup;
 
-    @ManyToMany((type) => Project, (project) => project.users)
-    public projects: Project[];
+  @ManyToMany((type) => Project, (project) => project.users)
+  public projects: Project[];
 
-    @Column()
-    @CreateDateColumn()
-    public createdAt: Date;
+  @Column()
+  @CreateDateColumn()
+  public createdAt: Date;
 
-    @Column()
-    @UpdateDateColumn()
-    public updatedAt: Date;
+  @Column()
+  @UpdateDateColumn()
+  public updatedAt: Date;
 
-    @ManyToMany((type) => Notification, (notification) => notification.receivers)
-    public receivedNotifications: Notification[];
+  @ManyToMany((type) => Notification, (notification) => notification.receivers)
+  public receivedNotifications: Notification[];
 
-    @ManyToMany((type) => Notification, (notification) => notification.seenBy)
-    public seenNotifications: Notification[];
+  @ManyToMany((type) => Notification, (notification) => notification.seenBy)
+  public seenNotifications: Notification[];
 
-    @OneToMany((type) => Notification, (notification) => notification.creator)
-    public sentNotifications: Notification[];
+  @OneToMany((type) => Notification, (notification) => notification.creator)
+  public sentNotifications: Notification[];
 
-    @OneToMany((type) => Message, (message) => message.sender)
-    public sentMessages: Message[];
+  @OneToMany((type) => Message, (message) => message.sender)
+  public sentMessages: Message[];
 
-    @OneToMany((type) => Message, (message) => message.toUser)
-    public receivedMessages: Message[];
+  @OneToMany((type) => Message, (message) => message.toUser)
+  public receivedMessages: Message[];
 
-    @OneToMany((type) => Template, (template) => template.creator)
-    public templates: Template[];
+  @OneToMany((type) => Template, (template) => template.creator)
+  public templates: Template[];
 
-    @OneToMany((type) => Tutorial, (tutorial) => tutorial.creator)
-    public tutorials: Tutorial[];
+  @OneToMany((type) => Tutorial, (tutorial) => tutorial.creator)
+  public tutorials: Tutorial[];
 
-    @OneToMany((type) => Event, (event) => event.creator)
-    public events: Event[];
+  @OneToMany((type) => Event, (event) => event.creator)
+  public events: Event[];
 
-    @OneToMany((type) => File, (file) => file.creator)
-    public files: File[];
+  @OneToMany((type) => File, (file) => file.creator)
+  public files: File[];
 
-    public hashPassword() {
-      this.password = bcrypt.hashSync(this.password, 8);
-    }
-
-    public checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
-        if (unencryptedPassword) {
-            return bcrypt.compareSync(unencryptedPassword, this.password);
-        } else {
-            return false;
-        }
-    }
+  public hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
   }
+
+  public checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+      if (unencryptedPassword) {
+          return bcrypt.compareSync(unencryptedPassword, this.password);
+      } else {
+          return false;
+      }
+  }
+}
