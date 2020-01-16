374bcb81309f113d325210880beb20aa6fa24f7c
Hannes Rüger <hannesrueger@gmx.de>
2020-01-15 16:17:07 -1000

    added create secrets script (api)

---
+++ api/src/scripts/generateSecretFilesWithKeys.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const path = require("path");
+const content = 'export const config = {    avalibleDiskSpaceInGB: 0,    cacheExpireDays: 0,    database_host: "",    database_name: "",    database_password: "",    database_port: 0,    database_user: "",    emailSender: "",    email_auth_pass: "",   email_auth_user: "",    email_host: "",    email_port: 0,    googleMapsApiKey: "",    jwtSecret: "",    storagePath: "",    tempFilesStoragePath: "",    templateFilesStoragePath: "",    tutorialFilesStoragePath: "",    url: ``,    url_domain: "",    url_path: "",    url_protocoll: "",};';
+fs.writeFileSync(path.join(__dirname, "../config/config.ts"), content);
