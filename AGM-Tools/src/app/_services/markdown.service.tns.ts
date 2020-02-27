a7a311e2d42945c6bbe271f1b5ff316e37fc6757
H. Rüger <hannesrueger@gmx.de>
2020-02-27 12:24:53 -1000

    fix android build for now

---
+++ AGM-Tools/src/app/_services/markdown.service.tns.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class MarkdownService {
+    public from(content: string) {
+        return undefined;
+    }
+    public to(content: string) {
+        return undefined;
+    }
+}
