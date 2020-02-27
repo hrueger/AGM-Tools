123fa60346409f50aa2545536b385d11248d8607
H. Rüger <hannesrueger@gmx.de>
2020-02-27 12:20:07 -1000

    added to html conversion

--- AGM-Tools/src/app/_services/markdown.service.ts
+++ AGM-Tools/src/app/_services/markdown.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
 import * as TurndownService from "turndown";
+import { MarkdownService as MdService } from "ngx-markdown";
 
 @Injectable()
 export class MarkdownService {
+    constructor(private mdService: MdService) {}
     public from(content: string) {
         // @ts-ignore
         return TurndownService.default().turndown(content) as string;
+    }
+    public to(content: string) {
+        return this.mdService.compile(content);
     }
 }
