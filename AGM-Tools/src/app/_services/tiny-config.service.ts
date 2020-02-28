c1f28a00a51884b98ee5ecd95d13a66451a0f1f1
H. Rüger <hannesrueger@gmx.de>
2020-02-28 07:46:10 -1000

    fixed tinymce when installed in subfolder

--- AGM-Tools/src/app/_services/tiny-config.service.ts
+++ AGM-Tools/src/app/_services/tiny-config.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from "@angular/core";
 import { FastTranslateService } from "./fast-translate.service";
+import { environment } from "~/environments/environment";
 
 @Injectable()
 export class TinyConfigService {
     constructor(private fts: FastTranslateService) {}
     public get() {
         return {
-            base_url: 'assets/tinymce',
-            suffix: '.min',
+            base_url: `${environment.appUrl.replace("/#/", "/")}assets/tinymce"`,
+            suffix: ".min",
             statusbar: false,
             menubar: false,
             language: this.fts.getLang(),
             plugins: [
-                'advlist autolink lists link searchreplace hr insertdatetime paste'
+                "advlist autolink lists link searchreplace hr insertdatetime paste"
             ],
             toolbar:
-                'undo redo | formatselect bold italic | link hr | bullist numlist outdent indent | insertdatetime'
+                "undo redo | formatselect bold italic | link hr | bullist numlist outdent indent | insertdatetime"
         };
     }
 }
