e1f3aecbb935b19f93a1f0946c2b0d40e9cff499
H. Rüger <hannesrueger@gmx.de>
2020-02-28 07:48:21 -1000

    fixed tinymce config

--- AGM-Tools/src/app/_services/tiny-config.service.ts
+++ AGM-Tools/src/app/_services/tiny-config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { FastTranslateService } from "./fast-translate.service";
-import { environment } from "~/environments/environment";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class TinyConfigService {
