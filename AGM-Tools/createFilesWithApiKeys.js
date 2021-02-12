b1d44bd64d6aec23c5efc45f0c8853dff72a8d5c
Hannes Rüger <hannesrueger@gmx.de>
2021-02-12 09:34:53 +0100

    updated build process

--- AGM-Tools/createFilesWithApiKeys.js
+++ AGM-Tools/createFilesWithApiKeys.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 
 const files = {
-    "src/environments/environment.ts": "/* eslint-disable */\nexport const environment = {apiUrl: \"\", defaultLanguage: \"\", documentServerUrl: \"\", appUrl: \"\", firebase_apiKey: \"\",firebase_appId: \"\",firebase_authDomain: \"\",firebase_databaseURL: \"\",firebase_messagingSenderId: \"\",firebase_projectId: \"\",firebase_storageBucket: \"\",production: false};",
-    "src/environments/environment.prod.ts": "/* eslint-disable */\nexport const environment = {apiUrl: \"\", defaultLanguage: \"\", documentServerUrl: \"\", appUrl: \"\", firebase_apiKey: \"\",firebase_appId: \"\",firebase_authDomain: \"\",firebase_databaseURL: \"\",firebase_messagingSenderId: \"\",firebase_projectId: \"\",firebase_storageBucket: \"\",production: true};",
     "src/firebase-messaging-sw.js": "importScripts(\"https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js\");importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');firebase.initializeApp({\"messagingSenderId\": \"\"});const messaging = firebase.messaging();",
     "App_Resources/Android/google-services.json": "{\"project_info\":{\"project_number\":\"\",\"firebase_url\":\"\",\"project_id\":\"\",\"storage_bucket\":\"\"},\"client\":[{\"client_info\":{\"mobilesdk_app_id\":\"\",\"android_client_info\":{\"package_name\":\"de.multimediaag.agmtools\"}},\"oauth_client\":[{\"client_id\":\"\",\"client_type\":3}],\"api_key\":[{\"current_key\":\"\"}],\"services\":{\"appinvite_service\":{\"other_platform_oauth_client\":[{\"client_id\":\"\",\"client_type\":3}]}}}],\"configuration_version\":\"1\"}",
 };
