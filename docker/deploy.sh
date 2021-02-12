45ec5e8923b3d47ace16f2bf9debca5f8b0d433e
Hannes Rüger <hannesrueger@gmx.de>
2021-02-12 09:48:26 +0100

    fixed workflow

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,3 +1,2 @@
 PACKAGE_VERSION=$(cat ../AGM-Tools/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-docker login -u $1 -p $2
 docker push hrueger/agm-tools:v$PACKAGE_VERSION
